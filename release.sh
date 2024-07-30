962b3e7538ac11b7a7d44f16c8d286dd503b1dab
Arlo O'Keeffe <tiger@k5d.de>
2024-07-30 17:02:53 +0200

    create automated releases

---
+++ release.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#
+# This script:
+#   - creates a new tag
+#   - updates all the action references to the new tag
+#   - commits the changes
+#   - pushes a new release
+#
+# The tag pattern is YYMM.0.XXX:
+#   - YY: the year with two letters; e.g. 23 or 24
+#   - MM: the month with two letters; e.g. 05 or 12
+#   - XXX: an unique release number that starts counting at 101; e.g. 108 or 112
+# This results in unique version numbers like 2407.0.101 or 2312.0.105
+# that always go up and let you see how far behind you are.
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+MAJOR_MINOR=$(date -u +"%y%m")
+
+# sync local tags with info from remote
+git fetch \
+    --prune \
+    --prune-tags \
+    --tags \
+    --force
+
+# create an empty commit that will be amended later
+git commit \
+    --allow-empty \
+    --message "this commit will be amended later"
+
+for MICRO in $(seq 101 999); do
+
+    VERSION=${MAJOR_MINOR}.0.${MICRO}
+
+    if git tag "v${VERSION}"; then
+
+        bash update-action-references.sh "v${VERSION}"
+
+        # commit the changes
+        git commit \
+            --amend \
+            --all \
+            --message "update references to v${VERSION}"
+
+        # update the tag reference to the latest commit before pushing
+        git tag \
+            --force \
+            "v${VERSION}"
+
+        if git push origin "v${VERSION}"; then
+            break
+        fi
+
+        # remove the local tag if the push failed
+        git tag --delete "v${VERSION}"
+
+    fi
+
+done
---
+++ update-action-references.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script updates all the action references to the first parameter.
+# The second parameter is the name of the referenced repository.
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+reference=${1:-${GITHUB_HEAD_REF:-${GITHUB_REF_NAME:-$(git rev-parse --abbrev-ref HEAD)}}}
+# escape all forward slashes with a backslash
+reference=${reference//\//\\/}
+
+repository=${2:-${GITHUB_REPOSITORY:-ArloL/github-actions-shared-workflows}}
+# escape all forward slashes with a backslash
+repository=${repository//\//\\/}
+
+# update the version of every "local" action reference with sed
+# in detail:
+# s     <- substitute command
+# |     <- first delimiter character; now comes the search regex
+#   (   <- start first group
+#   \s*  <- match all whitespace characters
+#   uses: ${repository}   <- literal string match
+#   [^@]+   <- match all characters except @
+#   )   <- close first group
+#   @   <- literal string match for the separator
+#   (.*)    <- second group that matches everything after the @
+# |     <- second delimiter character; now comes the replacement
+#   \1  <- copies the whole text from the first group match
+#   @   <- the separator between the action reference and the tag
+#   v${VERSION}     <- the new version
+# |     <- final delimiter character; finishes the replacement string
+# g     <- global flag to replace every occurence
+hash gsed 2>/dev/null && SED='gsed' || SED='sed'
+${SED} -i \
+    -E \
+    "s|(\s*uses: ${repository}[^@]+)@(.*)|\1@${reference}|g" \
+    .github/workflows/*.y*ml
